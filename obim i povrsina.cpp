7c9b512725dea8fabd0874611d48db041c9ec771
wirel3ssless <91086837+wirel3ssless@users.noreply.github.com>
2021-09-24 19:01:25 +0200

    Add files via upload

---
+++ obim i povrsina.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int a,b;
+	cout<<"Unesite stranice"<<endl;
+	cin>>a>>b;
+	int p=a*b;
+	int o=2*(a+b);
+	cout<<"Povrsina je " <<p<<" a obim je "<<o ;
+	return 0;
+}
